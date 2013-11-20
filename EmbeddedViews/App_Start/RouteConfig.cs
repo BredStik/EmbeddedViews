488765e3aa01efb12ec872720e9129201a2409cf
Mathieu Lafontaine <mathieulafontaine@hotmail.com>
2013-11-19 23:16:41 -0500

    Initial commit
    Embedded controllers, views and resources in MVC

---
+++ EmbeddedViews/App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using MvcCodeRouting;
+
+namespace EmbeddedViews
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
+            routes.MapCodeRoutes(
+                baseRoute: "hello",
+              rootController: typeof(MyPlugIn.Controllers.HelloController),
+              settings: new CodeRoutingSettings
+              {
+                  EnableEmbeddedViews = true,
+                  UseImplicitIdToken = true
+              }
+           );
+        }
+    }
+}
---
+++ EmbeddedViews/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmbeddedViews.MvcApplication" Language="C#" %>
